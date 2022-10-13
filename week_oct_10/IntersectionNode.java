6658c63ccec2bc457e37dde218060e4f15b18c83
ali alrubaye <alialrubaye1990@hotmail.com>
2022-10-12 21:09:04 -0400

    oct_10 weekly coding challenges solution

---
+++ week_oct_10/IntersectionNode.java
@@ -0,0 +1,69 @@
+package week_oct_10;
+
+import java.util.LinkedList;
+
+public class IntersectionNode {
+
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static String checkIntersection(LinkedList a, LinkedList b) {
+
+		String result = "No intersection found!";
+
+		for (int i = 0; i < a.size(); i++) {
+			for (int j = 0; j < b.size(); j++) {
+				if (a.get(i) == b.get(j)) {
+					result = "Intersection point is: (" + i + "," + j + ") and the value is: " + a.get(i);
+				}
+			}
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+
+		LinkedList a = new LinkedList<>();
+		LinkedList b = new LinkedList<>();
+
+		// There is one point of intersection example
+		a.add(1);
+		a.add(2);
+		a.add(3);
+		a.add(4);
+
+		b.add(5);
+		b.add(6);
+		b.add(7);
+		b.add(8);
+		b.add(4);
+
+		System.out.println(checkIntersection(a, b));
+
+		// There is no intersection example
+		a.clear();
+		b.clear();
+		a.add(1);
+		a.add(2);
+		a.add(3);
+		a.add(4);
+
+		b.add(5);
+		b.add(6);
+		b.add(7);
+
+		System.out.println(checkIntersection(a, b));
+
+	}
+
+}
+
+//Console output:
+//===============
+
+//Intersection point is: (3,4) and the value is: 4
+//No intersection found!
---
+++ week_oct_10/LoopDetector.java
@@ -0,0 +1,61 @@
+package week_oct_10;
+
+import java.util.LinkedList;
+
+public class LoopDetector {
+
+	/**
+	 * Method to check whether a LinkedList is circular and returns 
+	 * @param a -> The LinkedList to test
+	 * @return
+	 */
+	public static String detect(LinkedList a) {
+		
+		String result = "Not a circular LinkedList!";
+		LinkedList<Object> tmp = new LinkedList<Object>();
+		for (int i=0; i<a.size(); i++) {
+			if (tmp.contains(a.get(i))) {
+				result = "Circular LinkedList detected, node is: ("+i+") and the value is: "+a.get(i);
+				break;
+			} else {
+				tmp.add(a.get(i));
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+
+		LinkedList a = new LinkedList<>();
+		LinkedList b = new LinkedList<>();
+
+		// Circular example
+		a.add("A");
+		a.add("B");
+		a.add("C");
+		a.add("D");
+		a.add("E");
+		a.add("C");
+
+		System.out.println(detect(a));
+
+		// None circular example
+		b.add("A");
+		b.add("B");
+		b.add("C");
+		b.add("D");
+		b.add("E");
+		b.add("F");
+
+		System.out.println(detect(b));
+
+	}
+
+}
+
+//Console output:
+//===============
+
+//Circular LinkedList detected, node is: (5) and the value is: C
+//Not a circular LinkedList!
+
